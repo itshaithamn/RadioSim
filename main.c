7251bcd1bba437167f95d7dc74fd0681089f56cb
itshaithamn <hnaksho@oakland.edu>
2025-07-29 02:13:08 -0400

    going to create a new ui system based on ascii instead

--- main.c
+++ main.c
@@ -1,81 +1,16 @@
-#ifndef UNICODE
-#define UNICODE
-#endif
+#include <stdio.h>
+#include <math.h>
 
-#include <windows.h>
+int main() {
+    //rows then columns
+    char grid[10][10];
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    //parametric equations
+    int x = 5 + 5*cos(0.0);
+    int y = 5 + 5*sin(0.0);
+    printf("%d", x);
+    printf("%d", y);
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-{
-    // Register the window class.
-    const wchar_t CLASS_NAME[]  = L"Main";
-
-    WNDCLASS wc = { };
-
-    wc.lpfnWndProc   = WindowProc;
-    wc.hInstance     = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClass(&wc);
-
-    // Create the window.
-
-    HWND hwnd = CreateWindowEx(
-            0,                              // Optional window styles.
-            CLASS_NAME,                     // Window class
-            L"RadarSim",    // Window text
-            WS_OVERLAPPEDWINDOW,            // Window style
-
-            // Size and position
-            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-            NULL,       // Parent window
-            NULL,       // Menu
-            hInstance,  // Instance handle
-            NULL        // Additional application data
-    );
-
-    if (hwnd == NULL)
-    {
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-
-    // Run the message loop.
-
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam){
-    switch (uMsg)
-    {
-        case WM_DESTROY:
-            PostQuitMessage(0);
-            return 0;
-
-        case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            // All painting occurs here, between BeginPaint and EndPaint.
-            //This coding shit pissing me off
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
-
-
-
-            EndPaint(hwnd, &ps);
-        }
-        return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    //going to use the array to "set" the positions of the x and y coordinate, empty values will be replaced with spaces
+    //need to also set up a new line system as well and figure that shit out.
 }
